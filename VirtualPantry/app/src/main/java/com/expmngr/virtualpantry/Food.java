73f503fd531375b6bc650bb14eb519fd2e718621
Rik Karmakar <karri390@student.otago.ac.nz>
2019-04-04 15:25:07 +1300

    #1 Food domain class made

---
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/Food.java
@@ -0,0 +1,49 @@
+package com.expmngr.virtualpantry;
+
+public class Food {
+    private int id;
+    private String name;
+    private int quantity;
+    private String category;
+    private float time_till_expiry;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getTime_till_expiry() {
+        return time_till_expiry;
+    }
+
+    public void setTime_till_expiry(float time_till_expiry) {
+        this.time_till_expiry = time_till_expiry;
+    }
+}
