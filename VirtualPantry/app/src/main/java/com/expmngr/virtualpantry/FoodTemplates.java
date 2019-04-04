810282d1205951c0b1508ed1a12a4ad31dbfdfc7
Rik Karmakar <karri390@student.otago.ac.nz>
2019-04-04 15:35:23 +1300

    #2 FoodTemplates class made

---
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/FoodTemplates.java
@@ -0,0 +1,31 @@
+package com.expmngr.virtualpantry;
+
+public class FoodTemplates {
+    private String longName;
+    private String shortName;
+    private String category;
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
