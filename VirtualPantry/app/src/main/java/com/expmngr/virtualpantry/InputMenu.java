b7672eaa42b8a8f1696f40843911a96b77e325f1
morpt925 <morpt925@student.otago.ac.nz>
2019-04-04 15:38:39 +1300

    Added input menu frame

---
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/InputMenu.java
@@ -0,0 +1,13 @@
+package com.expmngr.virtualpantry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_menu);
+    }
+}
