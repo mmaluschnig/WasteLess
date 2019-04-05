3a67ce03f457df87f49aa5a478c94d45a768f0a1
morpt925 <morpt925@student.otago.ac.nz>
2019-04-05 21:47:47 +1300

    Created Add New Items Screen skeleton

---
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AddNewItems.java
@@ -0,0 +1,14 @@
+package com.expmngr.virtualpantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_items);
+    }
+}
