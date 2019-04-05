1147d7aaf21ce8f8c4d9157a522bc09dd0bc0731
Rik Karmakar <karri390@student.otago.ac.nz>
2019-04-05 16:00:39 +1300

    added some buttons to test the app

--- VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/InputMenu.java
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/InputMenu.java
@@ -1,7 +1,11 @@
 package com.expmngr.virtualpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InputMenu extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class InputMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_menu);
+
+        Button pantryUpdate = (Button) findViewById(R.id.pantryUpdate);
+        pantryUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), test.class);
+                startActivity(intent);
+            }
+        });
     }
 }
