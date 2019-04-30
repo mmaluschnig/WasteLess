4837e39b3878a668c3a85a02d5a60419ef0f02b9
Mitchie Maluschnig <mitchie.maluschnig@gmail.com>
2019-05-01 11:24:54 +1200

    added a new entity to the database which will hold expiry times of food

---
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/Database/DataAccessObjects/ExpiryFoodDAO.java
@@ -0,0 +1,22 @@
+package com.expmngr.virtualpantry.Database.DataAccessObjects;
+
+import com.expmngr.virtualpantry.Database.Entities.ExpiryFood;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
+public interface ExpiryFoodDAO {
+
+    @Insert
+    void addExpFood(ExpiryFood food);
+
+    @Query("select * from ExpiryFood")
+    List<ExpiryFood> getAllExpFood();
+
+    @Query("DELETE FROM ExpiryFood")
+    void deleteAll();
+}
