07dec2f74846f6d59e50fbaaefcf29b29b755c8c
Tim Offen <offentim@gmail.com>
2019-05-02 09:50:55 +1200

    Deleted the vector images and added images instead, also re-named the files (refactored) sweet #19

--- VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/Utils/BottomNavigationViewHelper.java
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/Utils/BottomNavigationViewHelper.java
@@ -1,11 +1,30 @@
 package com.expmngr.virtualpantry.Utils;
 
+import android.content.Context;
+import android.view.MenuItem;
+
+import com.expmngr.virtualpantry.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import androidx.annotation.NonNull;
 
 public class BottomNavigationViewHelper {
 
     public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
 
     }
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view){
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               switch (item.getItemId()){
 
+               }
+
+
+                return false;
+            }
+        });
+    }
 }
