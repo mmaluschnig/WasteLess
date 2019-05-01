21c015c99d345e748c0d68646e8d01da8c227bc7
RIK <rik.goku@gmail.com>
2019-05-01 14:13:50 +1200

    Empty Shopping list activity added to the project

---
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AppScreens/ShoppingList.java
@@ -0,0 +1,14 @@
+package com.expmngr.virtualpantry.AppScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoppinglist);
+    }
+}
