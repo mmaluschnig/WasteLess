ab6917a00308a1c33db707c50b2cfbb5db7b54e9
Tim Offen <offentim@gmail.com>
2019-05-08 20:47:01 +1200

    Started the splash screen

--- VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AppScreens/SplashScreen.java
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AppScreens/SplashScreen.java
@@ -1,4 +1,31 @@
 package com.expmngr.virtualpantry.AppScreens;
 
-public class SplashScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.expmngr.virtualpantry.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(), MainMenuPlaceholder.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
 }
