5a353cd6477a3ba9e7a9813d4d203b71ba3cd20f
Mitchie Maluschnig <mitchie.maluschnig@gmail.com>
2019-05-10 15:30:19 +1200

    updated the database version number after merge changed schema

--- VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/Database/FoodDatabase.java
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/Database/FoodDatabase.java
@@ -14,7 +14,7 @@ import com.expmngr.virtualpantry.Database.DataAccessObjects.FoodTemplatesDAO;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Food.class, ExpiryFood.class, FoodTemplates.class, FoodGroup.class, ExpiredFood.class}, version = 5)
+@Database(entities = {Food.class, ExpiryFood.class, FoodTemplates.class, FoodGroup.class, ExpiredFood.class}, version = 6)
 public abstract class FoodDatabase extends RoomDatabase {
     public abstract FoodDAO foodDAO();
     public abstract ExpiryFoodDAO expiryFoodDAO();
