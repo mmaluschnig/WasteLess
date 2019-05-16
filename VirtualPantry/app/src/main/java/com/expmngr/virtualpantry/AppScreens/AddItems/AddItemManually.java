a6402305de29f438658f62a451438df78c57f0a6
mmaluschnig <mitchie.maluschnig@mail.com>
2019-05-16 15:14:51 +1200

    added validation checking to manual add food and added a calendar selection tool for date selection

--- VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AppScreens/AddItems/AddItemManually.java
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AppScreens/AddItems/AddItemManually.java
@@ -2,6 +2,8 @@ package com.expmngr.virtualpantry.AppScreens.AddItems;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -22,6 +25,7 @@ import com.expmngr.virtualpantry.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class AddItemManually extends AppCompatActivity {
@@ -33,6 +37,7 @@ public class AddItemManually extends AppCompatActivity {
     EditText foodTimeTillExpiryText;
 
     private String[] locations = {"Pantry", "Fridge", "Freezer"};
+    final Calendar myCalendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,23 +52,50 @@ public class AddItemManually extends AppCompatActivity {
         addfoodManuallyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Food newFood = new Food();
-                newFood.setName(foodNameText.getText().toString());
-                newFood.setCategory(foodCategorytext.getText().toString());
-                newFood.setQuantity(Float.parseFloat(foodQuantityText.getText().toString()));
-                newFood.setLocation(foodLocationSpinner.getSelectedItem().toString());
-                newFood.setDate_added(new SimpleDateFormat("dd/MM/yyyy HH").format(new Date()));
-                newFood.setExpiryDate(foodTimeTillExpiryText.getText().toString() + " 00");
+                String name = foodNameText.getText().toString();
+                String category = foodCategorytext.getText().toString();
+                Float quantity = 0f;
+                String quantityMessage = "";
                 try {
-                    MainMenuPlaceholder.database.foodDAO().addFood(newFood);
-                }finally {
-                    foodNameText.setText("");
-                    foodCategorytext.setText("");
-                    foodQuantityText.setText("");
-                    foodTimeTillExpiryText.setText("");
-                    foodLocationSpinner.setSelection(0);
-                    Toast.makeText(AddItemManually.this, "Added: " + newFood.getName(), Toast.LENGTH_SHORT).show();
+                    quantity = Float.parseFloat(foodQuantityText.getText().toString());
+
+                }catch (Exception e){
+                    quantityMessage = e.getMessage();
+                }
+                String location = foodLocationSpinner.getSelectedItem().toString();
+                String addedDate = new SimpleDateFormat("dd/MM/yyyy HH").format(new Date());
+                String expiryDate = foodTimeTillExpiryText.getText().toString() + " 00";
+
+                //Check that fields are correct
+                if(name.equals("")){
+                    Toast.makeText(AddItemManually.this, "Food must have a Name", Toast.LENGTH_SHORT).show();
+                }else if(!quantityMessage.equals("")){
+                    Toast.makeText(AddItemManually.this, "Invalid Quantity", Toast.LENGTH_SHORT).show();
+                } else if(quantity <= 0){
+                    Toast.makeText(AddItemManually.this, "Quantity must be greater that 0", Toast.LENGTH_SHORT).show();
+                }else if(category.equals("")){
+                    Toast.makeText(AddItemManually.this, "Food must have a category", Toast.LENGTH_SHORT).show();
+                }else if(expiryDate.equals(" 00")){
+                    Toast.makeText(AddItemManually.this, "Food must have an expiry date", Toast.LENGTH_SHORT).show();
+                }else {
+                    //everything verifies
+                    Food newFood = new Food();
+                    newFood.setName(name);
+                    newFood.setCategory(category);
+                    newFood.setQuantity(quantity);
+                    newFood.setLocation(location);
+                    newFood.setDate_added(addedDate);
+                    newFood.setExpiryDate(expiryDate);
+                    try {
+                        MainMenuPlaceholder.database.foodDAO().addFood(newFood);
+                    } finally {
+                        foodNameText.setText("");
+                        foodCategorytext.setText("");
+                        foodQuantityText.setText("");
+                        foodTimeTillExpiryText.setText("");
+                        foodLocationSpinner.setSelection(0);
+                        Toast.makeText(AddItemManually.this, "Added: " + newFood.getName(), Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
@@ -83,7 +115,7 @@ public class AddItemManually extends AppCompatActivity {
         foodNameText = (EditText) findViewById(R.id.foodNameEditText);
         foodCategorytext = (EditText) findViewById(R.id.foodCategoryEditText);
         foodQuantityText = (EditText) findViewById(R.id.foodQuantityEditText);
-        foodTimeTillExpiryText = (EditText) findViewById(R.id.foodTimeTillExpiryEditText);
+        setUpExpiryDateInput();
 
         foodLocationSpinner = (Spinner) findViewById(R.id.foodLocationSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.food_locations, android.R.layout.simple_spinner_item);
@@ -91,6 +123,32 @@ public class AddItemManually extends AppCompatActivity {
         foodLocationSpinner.setAdapter(adapter);
 
 
+    }
+
+    private void setUpExpiryDateInput(){
+        foodTimeTillExpiryText = (EditText) findViewById(R.id.foodTimeTillExpiryEditText);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String myFormat = "dd/MM/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
+
+                foodTimeTillExpiryText.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+
+        foodTimeTillExpiryText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddItemManually.this, android.R.style.Theme_DeviceDefault_Dialog_Alert, date,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 
     private void setupBottomNavigationView() {
