130598293409ff85926839d1ab99edc49d64cb29
RIK <rik.goku@gmail.com>
2019-05-19 15:18:50 +1200

    removed useless bits

--- VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AppScreens/NotificationSettings.java
+++ VirtualPantry/app/src/main/java/com/expmngr/virtualpantry/AppScreens/NotificationSettings.java
@@ -28,17 +28,6 @@ public class NotificationSettings extends AppCompatActivity {
             Date c = getInstance().getTime();
             SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
             String todaysDate = df.format(c);
-            String todaysday = todaysDate.substring(0,2);
-            String todaysmonth = todaysDate.substring(3,6);
-            String todaysyear = todaysDate.substring(6,10);
-            String expday = expdate.substring(0,2);
-            String expmonth = expdate.substring(3,6);
-            String expyear = expdate.substring(6,10);
-
-            if((Integer.parseInt(expyear) == Integer.parseInt(todaysyear)) && Integer.parseInt(expmonth) == Integer.parseInt(todaysmonth)){
-
-               if(Integer.parseInt(expday) > Integer.parseInt(todaysday)){
-                   int diff = calculateDifference(Integer.parseInt(expday), Integer.parseInt(todaysday));
 
 
                }
@@ -47,15 +36,5 @@ public class NotificationSettings extends AppCompatActivity {
 
 
         }
-    }
 
-    public int calculateDifference(int something, int somethingelse){
-        if(something>somethingelse){
-            return something-somethingelse;
-        }
-        else if(something < somethingelse){
-            return somethingelse-something;
-        }
-        else return 0;
-    }
-}
+
